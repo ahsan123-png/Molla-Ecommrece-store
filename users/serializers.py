55e0e1b11dc4034302ac225d1301fca256e26bf6
ahsan123-png <135040587+ahsan123-png@users.noreply.github.com>
2024-01-28 20:14:42 +0500

    rolss

---
+++ users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from . import models
+# from .stripe import serializers as stripe_serializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserEx
+        fields = (
+            'id',
+            'username',
+            'email',
+            'phone',
+            "name",
+            'useraddress',
+            "gender",
+            "date_of_birth",
+            "date_joined"
+
+        )
