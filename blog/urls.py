0f7e75af0c844711cf3ce3f372f13b5bda0246fb
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-02-05 12:47:03 +0500

    changes

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views  # Import the home view
 
 urlpatterns = [
-    path('blog', views.view, name='blog'), 
-    path('review', views.review, name='review'), 
+    path('blog', views.blog, name='blog'), 
+    path('single', views.review, name='single'), 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-def view(request):
-    return render(request,"blog.html")
+
+def blog(request):
+    return render(request , "blog.html")
 
 def review(request):
     return render(request,"single.html")
+
