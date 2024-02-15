5d4bb514dc72e5c8175aac732f385bc13d317dcf
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-02-15 18:05:10 +0500

    product listing

--- product/urls.py
+++ product/urls.py
@@ -4,12 +4,11 @@ from django.urls import path
 from . import views  # Import the home view
 
 urlpatterns = [
-    path('lists', views.products, name='lists'), 
     path('detailed', views.product_details, name='detailed'), 
     path('add', views.add, name='add'), 
     path('add_product', views.addProduct, name='add_product'), 
     # ==== CURD =====
-    path('get_all', views.allProduct, name='allProduct'), 
+    path('lists', views.products, name='lists'), 
     path('get_product/<int:id>', views.getProduct, name='getProduct'), 
     path('update/<int:id>', views.updateProduct, name='updateProduct'), 
     path('delete/<int:id>', views.deleteProduct, name='deleteProduct'), 
