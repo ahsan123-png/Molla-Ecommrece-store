3c5217bea4ab38a4da74ad80506f8db87104e200
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-02-21 15:57:19 +0500

    add dashboard files

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),  # Add a URL pattern for the home view
+    path('admin', views.adminDashboard, name='adminDashboard'),  # Add a URL pattern for the home view
 ]
--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request,"dashboard.html")
+
+def adminDashboard(request):
+    return render(request,"admin/admin-dashboard.html")
