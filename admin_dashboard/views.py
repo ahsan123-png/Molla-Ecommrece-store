ab0033f70f1167fa9b2786006e00e2acd3ff6841
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-02-22 16:33:58 +0500

    add admin dashboard

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -5,4 +5,31 @@ def dashboard(request):
     return render(request,"dashboard.html")
 
 def adminDashboard(request):
-    return render(request,"admin/admin-dashboard.html")
+    return render(request,"admin/admindashboard.html")
+    
+def adminForms(request):
+    return render (request,"admin/forms.html")
+
+def adminIcons(request):
+    return render (request,"admin/icons.html")
+
+def adminLogin(request):
+    return render (request,"admin/login.html")
+
+def adminProfile(request):
+    return render (request,"admin/profile.html")
+
+def adminRegister(request):
+    return render (request,"admin/register.html")
+
+def adminResetPassword(request):
+    return render (request,"admin/reset-password.html")
+
+def adminTables(request):
+    return render (request,"admin/tables.html")
+
+def adminCalender(request):
+    return render (request,"admin/calendar.html")
+
+def adminBase(request):
+    return render (request,"admin/adminbase.html")
