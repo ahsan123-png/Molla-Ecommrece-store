0788de544b4fbb7d8dc2e07dd7c22f96edb26529
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-02-22 17:58:36 +0500

    checkout page

--- order/views.py
+++ order/views.py
@@ -1,8 +1,19 @@
+import json
 from django.shortcuts import render
+from django.http import QueryDict
+from urllib.parse import unquote
 
-# Create your views here.
-def chcekout(request):
-    return render(request,"checkout.html")
+def checkout(request):
+    if request.method == "GET":
+        # import pdb;pdb.set_trace()
+        cart_data = request.GET.get('cart_data', None)
+        initial_subtotal = request.GET.get('initial_subtotal', None)
+        if cart_data:
+            cart_data = unquote(cart_data)
+            # cart_data = QueryDict(cart_data)
+            cart_data = json.loads(cart_data)
+
+        return render(request, 'checkout.html', {'cart_data': cart_data, 'initial_subtotal': initial_subtotal})
 
 def shipment_address(request):
     return render(request,"checkout.html")
