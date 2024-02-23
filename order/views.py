bc955b9ba5397e2495165f2630585cafe90db78a
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-02-23 15:59:09 +0500

    checkout page fixes

--- order/views.py
+++ order/views.py
@@ -1,19 +1,23 @@
-import json
 from django.shortcuts import render
-from django.http import QueryDict
-from urllib.parse import unquote
+import ast
 
 def checkout(request):
     if request.method == "GET":
-        # import pdb;pdb.set_trace()
-        cart_data = request.GET.get('cart_data', None)
+        cart_data_encoded = request.GET.get('cart_data')
         initial_subtotal = request.GET.get('initial_subtotal', None)
-        if cart_data:
-            cart_data = unquote(cart_data)
-            # cart_data = QueryDict(cart_data)
-            cart_data = json.loads(cart_data)
-
+        cart_data_encoded = cart_data_encoded.replace("Decimal", "").replace("(", "").replace(")", "")
+        print("Encoded Cart Data:", cart_data_encoded)
+        if cart_data_encoded:
+            try:
+                cart_data = ast.literal_eval(cart_data_encoded)
+            except ValueError as e:
+                print(f"Error decoding JSON data: {e}")
+                cart_data = None
+            else:
+                print("Decoded Cart Data:", cart_data)  # Debug print
         return render(request, 'checkout.html', {'cart_data': cart_data, 'initial_subtotal': initial_subtotal})
+
+
 
 def shipment_address(request):
     return render(request,"checkout.html")
