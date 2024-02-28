d8ee635ec92abb3436d39486fe0f8b93d73d7e36
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-02-28 18:14:15 +0500

    order seen

--- order/views.py
+++ order/views.py
@@ -10,29 +10,21 @@ def checkout(request):
     if request.method == "GET":
         initial_subtotal = request.GET.get('initial_subtotal', None)
         user = request.user
-
-        # Fetch the cart items for the logged-in user
-        cart_items = Cart.objects.filter(user=user)
-
-        # Create an Order object for each cart item
+        if isinstance(user, UserEx):
+            user_ex = user
+        else:
+            user_ex = UserEx.objects.get(id=user.id)
+        cart_items = Cart.objects.filter(user=user_ex)
         for cart_item in cart_items:
             order = Order.objects.create(
-                customer=user,
+                customer=user_ex,
                 product=cart_item.product,
-                subtotal=cart_item.subtotal,  # Assuming total_price is a field in your Cart model
-                color=cart_item.color,
-                size=cart_item.size,
+                subtotal=cart_item.subtotal,
+                color=cart_item.selected_color,
+                size=cart_item.selected_size,
                 quantity=cart_item.quantity,
-                first_name=cart_item.first_name,
-                last_name=cart_item.last_name,
-                email=cart_item.email,
-                phone=cart_item.phone,
-                additional_note=cart_item.additional_note
-                # Add more fields as needed
             )
             order.save()
-
-        # Delete all cart items associated with the user
         cart_items.delete()
     return render(request, 'checkout.html', {'initial_subtotal': initial_subtotal})
 
