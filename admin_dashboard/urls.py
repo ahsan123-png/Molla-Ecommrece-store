54bb5b5bcf24d63dfb4ed189ebc9f852b239b549
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-03-16 23:33:50 +0500

    remove files

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),  # Add a URL pattern for the home view
-    path('admin', views.adminDashboard, name='adminDashboard'),  # Add a URL pattern for the home view
-    path('forms', views.adminForms, name='adminForms'),  # Add a URL pattern for the home view
-    path('icons', views.adminIcons, name='adminIcons'),  # Add a URL pattern for the home view
-    path('login', views.adminLogin, name='adminLogin'),  # Add a URL pattern for the home view
-    path('profile', views.adminProfile, name='adminProfile'),  # Add a URL pattern for the home view
-    path('register', views.adminRegister, name='adminRegister'),  # Add a URL pattern for the home view
-    path('calender', views.adminCalender, name='adminCalender'),  # Add a URL pattern for the home view
-    path('tables', views.adminTables, name='adminTables'),  # Add a URL pattern for the home view
-    path('base', views.adminBase, name='adminBase'),  # Add a URL pattern for the home view
+    path('', views.dashboard, name='dashboard'),
+    path('home_dashboard', views.homeDashboard, name='home_dashboard'), 
+    path('forms', views.adminForms, name='adminForms'), 
+    path('admin_login', views.adminLogin, name='adminLogin'),  
+    path('admin_register', views.adminRegister, name='adminRegister'),
+    path('tables', views.adminTables, name='adminTables'),
+    path('charts', views.adminChart, name='adminChart'),
 ]
--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,login,logout
 from django.views.decorators.csrf import csrf_exempt
+from product.models import Product
+from order.models import Order
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def dashboard(request):
     return render(request,"dashboard.html")
-
-def adminDashboard(request):
-    return render(request,"admin/admindashboard.html")
-    
-def adminForms(request):
-    return render (request,"admin/forms.html")
-
-def adminIcons(request):
-    return render (request,"admin/icons.html")
+# ======== access to dashboard  admin =====
+@login_required
+def homeDashboard(request):
+    if not request.user.is_authenticated:
+        return redirect('adminLogin')
+    else:
+        return render(request, 'admin/homeadmin.html')
+# ====== login admin ========= 
 @csrf_exempt
 def adminLogin(request):
     if request.method=="POST":
@@ -24,23 +26,22 @@ def adminLogin(request):
         if user is not None:
             login(request,user)
             return redirect("adminDashboard")
-    return render (request,"admin/login.html")
-
-def adminProfile(request):
-    return render (request,"admin/profile.html")
-
+    return render (request,"admin/signin.html")
+# ==== admin registeration ========
 def adminRegister(request):
-    
-    return render (request,"admin/register.html")
-
-def adminResetPassword(request):
-    return render (request,"admin/reset-password.html")
-
+    return render (request,"admin/signup.html")
+# ==== admin forms ========
+def adminForms(request):
+    return render (request,"admin/form.html")
+# ==== admin charts ========
+def adminChart(request):
+    return render (request,"admin/chart.html")
+# ===== dashboard tables ======
 def adminTables(request):
-    return render (request,"admin/tables.html")
+    return render (request,"admin/table.html")
+# ======= products and order counts ========
+@csrf_exempt
+def productCounts(request):
+    if request.method == "GET":
+        pass
 
-def adminCalender(request):
-    return render (request,"admin/calendar.html")
-
-def adminBase(request):
-    return render (request,"admin/adminbase.html")
