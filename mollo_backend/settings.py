7686088490e4c4ccfee393699c991494b963082a
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-06-16 14:23:56 +0500

    splite

--- mollo_backend/settings.py
+++ mollo_backend/settings.py
@@ -1,20 +1,15 @@
 from pathlib import Path
 import os
 from decouple import  config
-
+DEBUG=False
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
