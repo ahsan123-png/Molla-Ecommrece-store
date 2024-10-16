88ba65fc0678785c8df7101f8df6b3f79a08cd44
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-10-16 07:09:53 -0700

    Remove unwanted pycache files

--- mollo_backend/settings.py
+++ mollo_backend/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     "debug_toolbar"
     
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
